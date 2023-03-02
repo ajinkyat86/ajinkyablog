9b3aee572ab144de115b02c08da1582506f6ef7f
ajinkyablog <113884907+ajinkyablog@users.noreply.github.com>
2023-03-02 10:53:41 +0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   env: {
     "DEV_API_URL": "http://localhost:3000",
-    "PRODUCTION_API_URL": "https://www.ajinkyatanpure.com"
+    "PRODUCTION_API_URL": "https://www.ajinkyatanpure.com",
     "GOOGLE_VERIFICATION_ID": "dVGvocIbOJmFCN9CFWg2HGLXmGdZU8l_MKb78k7-HKA"
   }
 }
